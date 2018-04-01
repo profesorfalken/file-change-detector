97af8fa8e49bdbd3b87c5652967ca42ce4b9cd9e
profesorfalken <profesorfalken.github@gmail.com>
2018-04-01 22:21:47 +0200

    Add some ideas

--- src/main/java/com/profesorfalken/Main.java
+++ src/main/java/com/profesorfalken/Main.java
@@ -1,23 +1,40 @@
 package com.profesorfalken;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
 public class Main {
-    int pp;
+    public static final String BASE_DIRECTORY = "j:\\dev";
+
+    Map<String, WatchService> watchers = new HashMap<>();
+
     public static void main(String[] args) throws Exception {
-        Path path = Paths.get("J:\\dev");
+        Files.list(new File(BASE_DIRECTORY).toPath())
+                .filter(Files::isDirectory)
+                .forEach(path -> {
+                    System.out.println(path.getFileName());
+                });
+
+        /*
+        try (Stream<Path> paths = Files.walk(Paths.get(BASE_DIRECTORY))) {
+            paths.filter(Files::isDirectory)
+                    .forEach(System.out::println);
+        }*/
+
+/*
         WatchService watchService = path.getFileSystem().newWatchService();
         path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_CREATE);
-new Main().pp=2;
+
         WatchKey watchKey = null;
         while (true) {
             watchKey = watchService.poll();
@@ -25,7 +42,7 @@ new Main().pp=2;
                 watchKey.pollEvents().stream().forEach(event -> System.out.println("Modify file: " + event.context() + " Event: " + event.kind()));
                 watchKey.reset();
             }
-        }
+        }*/
     }
 // https://howtodoinjava.com/java-8/java-8-watchservice-api-tutorial/
 }
